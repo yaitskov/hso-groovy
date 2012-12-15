89168423f6c659e82eea206d1e56bf9a495b660e
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2012-12-15 22:41:10 +0400

    replace connection HTTP tester with ping ICMP

--- src/Tester.groovy
+++ src/Tester.groovy
@@ -2,12 +2,12 @@
 class Tester {
 
     static isConnected() {
-        try {
-            def url = "http://www.google.ru/?q=" + Util.passGen("abcdefghijqwervkl", 10)
-            println "Test connectivity with $url"
-            return url.toURL().text.length() > 0;
-        } catch (e) {
-            println e
+        def pingPro = "ping -c 1 8.8.4.4".execute()
+
+        if (pingPro.waitFor()) {
+            println pingPro.text
+            return false
         }
+        true
     }
 }
